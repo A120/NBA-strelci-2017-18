a3df0a2a944d8c09d07c3f9fc6e6764ec071323e
A120 <zaloznik.vito@hotmail.com>
2018-11-04 12:08:42 +0100

    Update naredi_csv.py

--- naredi_csv.py
+++ naredi_csv.py
@@ -7,7 +7,7 @@ vzorec = r'/nba/player/(.*?)">(.*?)</a>, ([A-Z]{1,2})</td><td align="left">(.*?)
 
     
 with open ("podatki.csv", "w") as csv:
-    print ("ime,ekipa,st_tekem,minute_na_tekmo,tocke,fgm-fga,fg%,3pm-3pa,3p%,ftm-fta,ft%", file=csv)
+    print ("ime,pozicija,ekipa,st_tekem,minute_na_tekmo,tocke,fgm-fga,fg%,3pm-3pa,3p%,ftm-fta,ft%", file=csv)
 
     for ujemanje in re.finditer (vzorec, vsebina, re.DOTALL):
         print('{},{},{},{},{},{},{},{},{},{},{},{}'.format(ujemanje.group(2), ujemanje.group(3), ujemanje.group(4), ujemanje.group(5), ujemanje.group(6), ujemanje.group(7), ujemanje.group(8), ujemanje.group(9), ujemanje.group(10), ujemanje.group(11), ujemanje.group(12), ujemanje.group(13)), file=csv)
